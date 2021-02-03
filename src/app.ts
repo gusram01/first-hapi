ab3f4541f3f96d2de262673b1ab4548082aab53d
Gus Ramírez <g.ram.bt@hotmail.com>
2021-02-03 11:32:58 -0600

    add inert to handle static files

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,35 @@
 import Hapi from '@hapi/hapi';
+import path from 'path';
+import inert from '@hapi/inert';
 
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || 'localhost';
+const routes: Hapi.RouteOptions = {
+  files: {
+    relativeTo: path.join(__dirname, '..', 'public'),
+  },
+};
 
 const init = async () => {
-  const server = Hapi.server({ port, host });
+  const server = Hapi.server({ port, host, routes });
+
+  await server.register(inert);
 
   server.route({
     method: 'GET',
-    path: '/',
-    handler: (req, h) => h.response('Hello World').code(200),
+    path: '/home',
+    handler: (req, h) => h.file('index.html'),
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: '.',
+        redirectToSlash: true,
+      },
+    },
   });
 
   await server.start();
